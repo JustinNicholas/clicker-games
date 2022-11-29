f25a791db3ce1c6abd62bb28c1bd1651d806ab4a
Justin Nicholas <nicholasj964@yahoo.com>
2022-11-29 10:49:29 -0600

    set up files

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Aim Trainer
+      </header>
+      <body>
+        <div className='game-box'>
+
+        </div>
+      </body>
+    </div>
+  );
+}
+
+export default App;
