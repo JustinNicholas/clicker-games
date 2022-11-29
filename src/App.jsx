21f4ac77a66a64f3b00df80350d26101a864fd9a
Justin Nicholas <nicholasj964@yahoo.com>
2022-11-29 11:19:29 -0600

    remove start onClick working

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,28 @@
 import './App.css';
 
 function App() {
+
+  const startGame = (bubble) => {
+    const element = document.getElementById('game-box')
+    for ( const child of element.children ){
+      console.log(child);
+      if (child.classList.contains('start')) {
+        element.removeChild(child)
+        console.log('removed');
+      }
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         Aim Trainer
       </header>
       <body>
-        <div className='game-box'>
-
+        <div id='game-box' className='game-box'>
+          <div className='start-bubble start'>
+            <p className='zero-margin' onClick={() => startGame()}>Start</p>
+          </div>
         </div>
       </body>
     </div>
